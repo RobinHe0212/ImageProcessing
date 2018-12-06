c632bf794aec2b6ad102585c2e0ce85a3d6a70ca
Robin He <robinhe@robins-macbook-pro.local>
2018-12-06 11:00:20 -0500

    Initial Commit

---
+++ ImageProcessing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageProcessing
+//
+//  Created by Robin He on 12/6/18.
+//  Copyright © 2018 Robin He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
